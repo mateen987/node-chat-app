c8967ca9edc97f426563493db6362c14c22d8f10
mateen987 <abdulmateen334@gmail.com>
2018-12-22 04:38:41 +0500

    adding emmit events

--- service/service.js
+++ service/service.js
@@ -1,9 +1,33 @@
 const path= require('path');
+const socketIo =require('socket.io');
+var http =require('http');
 const publicpath= path.join(__dirname, '../public');
 const express= require('express');
 const app=express();
+
+var server = http.createServer(app);
+var io= socketIo(server);
+
+
 const port= process.env.PORT || 3000;
 app.use(express.static(publicpath));
-app.listen(port,() => {
+
+io.on('connection',(socket)=>{
+    console.log('new user connected');
+
+    socket.emit('newMessage',{
+       from :'abdulmateen334@gmail.com',
+       text:'hello what`s up',
+       createAt:123,
+    })
+    socket.on('createMessage',(message)=>{
+        console.log('message created', message);
+    })
+    socket.on('disconnect',()=>{
+        console.log('user disconnected');
+    })
+    })
+
+server.listen(port,() => {
 console.log(`server is running ${port}`);
 });
