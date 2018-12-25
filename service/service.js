73e75f213b0d426b328852bb3267e5ff0d4d09ae
mateen987 <abdulmateen334@gmail.com>
2018-12-25 20:53:55 +0500

    add util function

--- service/service.js
+++ service/service.js
@@ -2,6 +2,7 @@ const path= require('path');
 const socketIo =require('socket.io');
 var http =require('http');
 const publicpath= path.join(__dirname, '../public');
+const {generateMessage}=require('./utils/message');
 const express= require('express');
 const app=express();
 
@@ -15,20 +16,16 @@ app.use(express.static(publicpath));
 io.on('connection',(socket)=>{
     console.log('new user connected');
 
-    // socket.emit('newMessage',{
-    //    from :'abdulmateen334@gmail.com',
-    //    text:'hello what`s up',
-    //    createAt:123,
-    // })
-    socket.on('createMessage',(message)=>{
-        console.log('message created', message);
-        io.emit('newMessage',{
-            text: message.text,
-            from:message.from,
-            createdAt:new Date().getTime()
+    socket.emit('newMessage', generateMessage('admin', 'welcome to chat app'));
+    socket.broadcast.emit('newMessage', generateMessage('admin', 'new user message'));
 
-        });
-    })
+       socket.on('createMessage',(message)=>{
+        console.log('message created', message);
+
+        io.emit('newMessage',generateMessage(message.from,message.text))
+
+    });
+
     socket.on('disconnect',()=>{
         console.log('user disconnected');
     })
